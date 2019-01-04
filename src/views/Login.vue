fcc6b551cade25d394b04d5489c2a5ad4cb1f254
Daniel Stefan Klose <daniel-stefan.klose@udo.edu>
2019-01-04 12:22:55 +0100

    fixed missing property

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -89,6 +89,7 @@ export default {
                   token
                   me {
                     name
+                    email
                     id
                     permissions
                     company {
